e5b637b10da8fb69a95adf74a184f9b62aba94d8
Edunogodz <edunogodz@gmail.com>
2020-03-12 14:06:25 -0300

    05-Class & Style Binding

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		product: 'Socks',		
-		inStock: true,
+		inStock: false,
 		image: './assets/vmSocks-green.jpg'	
 		,details: ["80% Cotton","20% polyester","Gender-neutral"]
 		,variants: [
